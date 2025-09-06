0a61932bd822ebef77b9e28abd54d18235722ac1
AntonyRegin <antonyregin7@gmail.com>
2025-09-06 06:53:33 +0530

    Update this

--- script.js
+++ script.js
@@ -39,11 +39,13 @@ const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
 const shuffleBtn = document.getElementById('shuffle');
 const progressBar = document.getElementById('progress');
+const repeatBtn = document.getElementById('repeat');
 let audio = new Audio();
 let playlist = [];
 let currentTrack = 0;
 let isPlaying = false;
 let isShuffling = false;
+let isRepeat = false;
 let shuffledOrder = [];
 
 // Try to fetch all .mp3 files from the songs folder
@@ -157,8 +159,19 @@ shuffleBtn.onclick = () => {
   renderPlaylist();
 };
 
+repeatBtn.onclick = () => {
+  isRepeat = !isRepeat;
+  repeatBtn.classList.toggle('active', isRepeat);
+};
+
+// Update next song logic to support repeat
 audio.addEventListener('ended', () => {
-  nextBtn.click();
+  if (isRepeat) {
+    audio.currentTime = 0;
+    playTrack();
+  } else {
+    nextBtn.click();
+  }
 });
 
 audio.addEventListener('timeupdate', () => {
